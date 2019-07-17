e626b30367d563fc26fa63d3c53060c60ed4ba68
Sindy Stefani <sindystefani7@gmail.com>
2019-07-17 16:05:32 +0700

    ui login

--- pages/auth0/login.vue
+++ pages/auth0/login.vue
@@ -7,7 +7,18 @@
       align-center
     >
       <v-flex xs12 sm6>
-        login
+        <div class="auth0--box">
+          <h3 class="mb-2">Login</h3>
+          <v-text-field 
+            label="Username" 
+            v-model="username"
+          />
+          <v-text-field 
+            label="Password" 
+            v-model="password"
+            type="password"
+          />
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
@@ -15,10 +26,13 @@
 </template>
 
 <script>
-import thumbnailMd from '~/components/zomato/thumbnailMd.vue'
-import search from '~/components/zomato/search.vue'
 import axios from 'axios'
 export default {
-
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  }
 }
 </script>
