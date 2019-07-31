ef7adfd540ec6fd59338cd803965b7b045ee4209
Sindy Stefani <sindystefani7@gmail.com>
2019-07-31 11:45:29 +0700

    create chat function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 const express = require('express')
 const { Nuxt } = require('nuxt')
 
@@ -23,3 +24,33 @@ function handleRequest(req, res) {
 }
 app.get('*', handleRequest)
 exports.nuxtApp = functions.https.onRequest(app)
+
+//function for chat
+admin.initializeApp()
+
+exports.onMessageCreate = functions.firestore
+  .document('messages/{userId}')
+  .onCreate((snap, context) => {
+    // Check if limit is reached
+    const newValue = snap.data();
+    console.log(newValue);
+    const ref = admin.firestore().collection('messages').orderBy('timestamp');
+
+    ref.onSnapshot(snapshot => {
+      i = 0;
+      size = snapshot.size;
+      sizeToDelete = size - 10;
+      console.log('Messages Count: ' + size);
+            
+      snapshot.forEach((doc) => {
+        if (i < sizeToDelete) {
+          doc.ref.delete().then(()=>{
+            console.log("Document successfully deleted");
+          }).catch((error) => {
+            console.log("Error removing document: ", error);
+          });
+        }
+        i++;  
+      });
+    });
+  });
