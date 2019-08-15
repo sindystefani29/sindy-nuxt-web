4b679c02ec5f48279917b44e08c9d4b40f161032
Sindy Stefani <sindystefani7@gmail.com>
2019-08-15 11:39:57 +0700

    hide secret are

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 let firebaseConfig = {
-  apiKey: "AIzaSyAdqS68LeShBYVTkEq2yKXh1NCbiox56kE",
-  authDomain: "trial-firebase-fa6e9.firebaseapp.com",
-  databaseURL: "https://trial-firebase-fa6e9.firebaseio.com",
-  projectId: "trial-firebase-fa6e9",
-  storageBucket: "gs://trial-firebase-fa6e9.appspot.com",
-  messagingSenderId: "703623261743",
-  appID: "1:703623261743:web:9f0e73efeceea5e0",
+  apiKey: "<SECRET AREA>",
+  authDomain: "<SECRET AREA>",
+  databaseURL: "<SECRET AREA>",
+  projectId: "<SECRET AREA>",
+  storageBucket: "<SECRET AREA>",
+  messagingSenderId: "<SECRET AREA>",
+  appID: "<SECRET AREA>",
 }
 
 if (!firebase.apps.length) {
